ad699c58cea3c78b144e481974492c91b29c4a25
gizemayegul <gizemayazyegul@gmail.com>
2023-01-30 16:43:54 +0100

    first

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () =>{
+    return(
+        <>
+        <ul>
+            <li>
+                Doormet Navigation
+            </li>
+            <li>
+                Hoem About
+            </li>
+        </ul>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,81 @@
+import { Box, HStack } from "@chakra-ui/react";
+const menu = [
+    {
+      url: "#Home",
+      name: "Home"
+    },
+    {
+      url: "#About",
+      name: "About"
+    },
+    {
+      url: "#Menu",
+      name:"Menu"
+    },
+    {
+      url: "#Reservations",
+      name:"Reservations"
+    },
+    {
+      url: "#OrderOnline",
+      name:"Order Online"
+    },
+    {
+      url: "#Login",
+      name:"Login"
+    }
+   
+  ];
+
+const Header = () =>{
+    const menuList = menu.map((item,index)=>{
+        return <Box key ={index} size="2px">
+      <ul>
+        <li className="menu">
+          <a href={item.url} onClick={item.url}>{item.name}</a>
+        </li>
+      </ul> 
+    </Box>
+  })
+    return(
+      <Box 
+        position="fixed"
+        top={0}
+        left={0}
+        right={0}
+        translateY = {0}
+        transitionProperty="transform"
+        transitionDuration=".3s"
+        transitionTimingFunction="ease-in-out"
+        backgroundColor="#FFFFFF"
+      >
+        
+        <Box color="white" maxWidth="1280px" margin="0 auto">
+          <HStack
+            px={16}
+            py={4}
+            justifyContent="space-between"
+            alignItems="center"
+
+          >
+            <nav>
+              {/* Add social media links based on the `socials` data */}
+              <HStack spacing='24px'>
+              <img src="Logo.svg" alt="logo"/>
+              </HStack>
+            </nav>
+            <nav>
+              <HStack spacing={8}>
+                {/* Add links to Projects and Contact me section */}
+                  {menuList}
+              </HStack>
+            </nav>
+          </HStack>
+        </Box>
+      </Box>
+    );
+
+
+}
+
+export default Header
